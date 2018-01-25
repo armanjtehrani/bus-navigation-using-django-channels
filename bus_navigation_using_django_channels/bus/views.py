cb1f35ccb3c4187aadaf5a9bc78af849c29a8b14
mammalofski <masahati.navid@gmail.com>
2018-01-25 11:36:44 +0330

    add new api to retreive group names

--- bus_navigation_using_django_channels/bus/views.py
+++ bus_navigation_using_django_channels/bus/views.py
@@ -1,3 +1,10 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
+from .models import Line
+
+
 # Create your views here.
+def group_list(request):
+    list_of_groups = list(Line.objects.all().values_list('name', flat=True))
+    return JsonResponse({'groups': list_of_groups})
